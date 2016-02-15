05b91439aa0b0d3bdfbff9ebef6ad28344f4a135
Lin He <he9lin@gmail.com>
2016-02-14 17:57:16 -0800

    Add util, mixins, service

--- app/register/route.js
+++ app/register/route.js
@@ -1,19 +1,26 @@
 import Ember from 'ember';
+import ValidationFunctions from 'ember-note/mixins/validation-functions';
 
-export default Ember.Route.extend({
-  actions: {
-    addNew: function() {
-      var user = this.store.createRecord('user', {
-        name: this.controller.get('name')
-      });
-      user.save().then(() => {
-        console.log('save successful');
-        this.controller.set('message', 'A new user with the name "'
-          + this.controller.get('name') + '" was added!');
-        this.controller.set('name',null);
-      }, function() {
-        console.log('save failed');
-      });
+export default Ember.Route.extend(ValidationFunctions,{
+  actions : {
+    addNew : function() {
+      var name = this.controller.get('name');
+      if(this.isValidEmail(name)) {
+        var user = this.store.createRecord('user', {
+          name: name
+        });
+        user.save().then(() => {
+          console.log('save successful');
+          this.controller.set('message',
+            'A new user with the name "' + name + '" was added!');
+          this.controller.set('name', null);
+        }, function() {
+          console.log('save failed');
+        });
+      }
+      else {
+        alert('Invalid email address.');
+      }
     }
   }
 });
