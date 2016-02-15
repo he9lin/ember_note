07ddedef5dc866ad470a90499ab774e99bef45f2
Lin He <he9lin@gmail.com>
2016-02-14 18:17:16 -0800

    Add unit and component tests

--- app/serializers/notebook.js
+++ app/serializers/notebook.js
@@ -1,9 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.RESTSerializer.extend({
-  normalize: function(typeClass, hash, prop) {
-    return this._super(typeClass, hash, prop);
-  },
+  // normalize: function(typeClass, hash, prop) {
+  //   return this._super(typeClass, hash, prop);
+  // },
 
   // serialize: function(snapshot, options) {
   //   return snapshot;
--- tests/unit/utils/is-valid-length-test.js
+++ tests/unit/utils/is-valid-length-test.js
@@ -5,6 +5,6 @@ module('Unit | Utility | is valid length');
 
 // Replace this with your real tests.
 test('it works', function(assert) {
-  let result = isValidLength();
+  let result = isValidLength("abc", 1, 5);
   assert.ok(result);
 });
